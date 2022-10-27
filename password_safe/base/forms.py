5adda2977460ec75a5f2aa58d750524bfd7929f3
Dcampos <dcampos@brssz.com>
2022-10-27 15:21:17 -0300

    fixed login and register pages

--- password_safe/base/forms.py
+++ password_safe/base/forms.py
@@ -3,34 +3,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit, MultiField, Div, Field
-
 class NewUserForm(UserCreationForm):
     username    = forms.CharField(
-        label='Username', 
+        label='', 
         widget=forms.TextInput(attrs={"class": "form-control mb-3", 'placeholder': 'Your Name'})
         )
     email       = forms.EmailField(
-        label='Email', 
+        required=True,
+        label='', 
         max_length=50, 
         widget=forms.EmailInput(attrs={"class":"form-control  mb-3",'placeholder': 'user@email.com'})
         )
     password1   = forms.CharField(
-        label='Password', 
-        widget=forms.PasswordInput(attrs={"class":"form-control mb-3"})
+        label='', 
+        widget=forms.PasswordInput(attrs={"class":"form-control mb-3", 'placeholder': 'Your password'})
         )
     password2   = forms.CharField(
-        label='Password confirmation',
-        widget=forms.PasswordInput(attrs={"class":"form-control mb-3"})
+        label='',
+        widget=forms.PasswordInput(attrs={"class":"form-control mb-3", 'placeholder': 'Password confirmation'})
         )
     class Meta:
         model  = User
         fields = ("username", "email", "password1", "password2")
         
     def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
+        user = super(NewUserForm, self).save(commit=True)
+        user.username = self.cleaned_data['username']
         user.email = self.cleaned_data['email']
+        user.password1 = self.cleaned_data['password1']
+        user.password2 = self.cleaned_data['password2']
+
+        
         if commit:
             user.save()
         return user
